2296bbd8a0e9c2bb1f298dbb77fea8ad7812af1d
Frehiwot Haile <54357776+she-code@users.noreply.github.com>
2023-03-03 17:31:37 +0530

    updated app.js

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const app = express();
 const userRoute = require("./routes/userRoute");
 const contactRoute = require("./routes/contactRoute");
 const globalErrorHandler = require("./controllers/errorController");
+
+app.get("/", (req, res) => {
+  res.send("welcome to contact app");
+});
 app.use(express.json());
 app.use(morgan("dev"));
 app.use("/api/users", userRoute);
